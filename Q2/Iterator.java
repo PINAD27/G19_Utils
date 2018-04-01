c53f949e51837139fe48cec1e601f5ec2b7b0f0d
Your Name <you@example.com>
2018-04-01 14:03:17 -0400

    removed dupe

--- Q2/Iterator.java
+++
@@ -1,5 +0,0 @@
-public interface Iterator<E> {
-    E next();
-    boolean hasNext();
-    int nextIndex();
-}
--- Q2/List.java
+++
@@ -1,8 +0,0 @@
-public interface List<E> {
-    void addFirst(E elem);
-    int size();
-    E get(int index);
-    Iterator<E> iterator();
-    Iterator<E> iterator(int nextIndex);
-    Iterator<E> iterator(Iterator<E> other);
-}
