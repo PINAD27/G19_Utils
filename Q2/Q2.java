b8c4abeab5a6adb82c9300882e5110eeb9eeebcb
Your Name <you@example.com>
2018-04-01 14:01:52 -0400

    iterator made

---
+++ Q2/Q2.java
@@ -0,0 +1,33 @@
+public class Q2 {
+
+    public static void main(String[] args) {
+
+        StudentInfo.display();
+
+        LinkedList<String> alphabet;
+        alphabet = new LinkedList<String>();
+
+        alphabet.add("alpha");
+        alphabet.add("bravo");
+        alphabet.add("charlie");
+        alphabet.add("delta");
+        alphabet.add("echo");
+
+        Iterator<String> i;
+        i = alphabet.iterator();
+        Iterator<String> k = alphabet.iterator(1);
+        Iterator<String> j = alphabet.iterator(k);
+        while (i.hasNext()) {
+            System.out.println(i.nextIndex());
+            System.out.println(i.next());
+        }
+        System.out.println(i.nextIndex());
+        System.out.println("\nk: " + k.next() + "\nj: " + j.next());
+        // while(k.hasNext()){
+        //   System.out.println(k.nextIndex());
+        //   System.out.println(k.next());
+        // }
+
+
+    }
+}
